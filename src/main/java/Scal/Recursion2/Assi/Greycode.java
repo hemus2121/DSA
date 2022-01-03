d7bb4bd2c064d858455aaf3ce770d5e031ca0566
hemanth <hemus2121@gmail.com>
2022-01-03 20:12:50 +0530

    greycode

---
+++ src/main/java/Scal/Recursion2/Assi/Greycode.java
@@ -0,0 +1,34 @@
+package Scal.Recursion2.Assi;
+
+/*Problem Description
+
+        The gray code is a binary numeral system where two successive values differ in only one bit.
+        Given a non-negative integer A representing the total number of bits in the code, print the sequence of gray code.
+        A gray code sequence must begin with 0.
+        */
+
+import java.util.ArrayList;
+
+public class Greycode {
+
+    static int num=0;
+    public static ArrayList<Integer> grayCode(int a) {
+          ArrayList<Integer> res = new ArrayList<>();
+          computeGreyCode(a, res);
+          return res;
+    }
+
+    private static void computeGreyCode(int a,ArrayList<Integer>res){
+        if (a==0) {
+            res.add(num);
+            return;
+        }
+        computeGreyCode(a-1, res);
+        num = num ^ (1<<(a-1));
+        computeGreyCode(a-1, res);
+    }
+    public static void main(String[] args) {
+        System.out.println(grayCode(2));
+
+    }
+}
