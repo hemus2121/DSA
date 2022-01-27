c3b1aac9d6bc20b4ede581f66d818ad75d688788
hemus2121 <hemus2121@github.com>
2022-01-27 22:03:56 +0530

    more ll prob

---
+++ src/main/java/Scal/LinkedList1/Assi/KReverseLinkedList.java
@@ -0,0 +1,32 @@
+package Scal.LinkedList1.Assi;
+
+import  Scal.LinkedList1.Assi.LinkedList1.ListNode;
+
+
+/*
+Problem Description
+Given a singly linked list A and an integer B, reverse the nodes of the list B at a time and
+ return modified linked list.
+ */
+public class KReverseLinkedList {
+
+    public ListNode reverseList(ListNode A, int B) {
+        return reverseInRecursion(A, B);
+    }
+
+    public ListNode reverseInRecursion(ListNode head, int B){
+        if (head == null ) return null;
+        ListNode prev=null, nxt=null,cur =head;
+         // most of it reversing list only and reverse first k nodes
+        while (cur!= null && B>0){
+            nxt = cur.next;
+            cur.next=prev;
+            prev = cur;
+            cur =nxt;
+            B--;
+        }
+        //invoke recursively for next set of nodes
+        head.next = reverseInRecursion(cur, B);
+        return prev;
+    }
+}
--- src/main/java/Scal/LinkedList1/Assi/ReveseLinkList2.java
+++ src/main/java/Scal/LinkedList1/Assi/ReveseLinkList2.java
@@ -1,5 +1,6 @@
 package Scal.LinkedList1.Assi;
 
+import  Scal.LinkedList1.Assi.LinkedList1.ListNode;
 /*
  Reverse a linked list A from position B to C.
 
@@ -8,6 +9,45 @@ NOTE: Do it in-place and in one-pass.
 public class ReveseLinkList2 {
 
     public ListNode reverseBetween(ListNode A, int B, int C) {
+        ListNode temp = A;
+        ListNode first=null,  from=null, to=null, last=null;
+        int count =0;
+        //Iterate to get point to B to C
+        while (temp !=null){
+            count++;
+            if (count < B){
+                first = temp;
+            }
+            if (count==B){
+                from = temp;
+            }
+            if (count ==C){
+                to = temp;
+                last = temp.next;
+                break;
+            }
+            temp = temp.next;
+        }
+        //make to nullify to.next to consider that as standalone list
+        to.next = null;
+        reverseList(from);
 
+        //attache last set of nodes after reversal
+        from.next = last;
+
+        if (first !=null) first.next=to;
+        else A =to;
+
+        return A;
+    }
+
+    private static void reverseList(ListNode head){
+        ListNode prev=null, nxt=null, cur = head;
+        while (cur !=null){
+            nxt =cur.next;
+            cur.next= prev;
+            prev = cur;
+            cur = nxt;
+        }
     }
 }
