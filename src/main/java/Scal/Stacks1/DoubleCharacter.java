dc8b140ed56b97888b4f16e72f378b43f6cdb8b8
Hemanth Babu H V <hemus2121@gmail.com>
2022-02-02 12:22:32 +0530

    stack prob

---
+++ src/main/java/Scal/Stacks1/DoubleCharacter.java
@@ -0,0 +1,43 @@
+package Scal.Stacks1;
+
+import java.util.Stack;
+
+/*
+Problem Description
+
+You are given a string A.
+
+An operation on the string is defined as follows:
+
+Remove the first occurrence of same consecutive characters. eg for a string "abbcd", the first occurrence of same consecutive characters is "bb".
+
+Therefore the string after this operation will be "acd".
+
+Keep performing this operation on the string until there are no more occurrences of same consecutive characters and return the final string.
+ */
+public class DoubleCharacter {
+    private static String solve(String A){
+        if (A==null) return null;
+        Stack <Character> stack = new Stack<>();
+        for (char c : A.toCharArray()){
+            if (stack.isEmpty()){
+                stack.push(c);
+            }else {
+                if (stack.peek()== c){
+                    stack.pop();
+                }else{
+                    stack.push(c);
+                }
+            }
+        }
+        //build response
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        return  sb.reverse().toString();
+    }
+    public static void main(String[] args) {
+        System.out.println(solve("abccbc"));
+    }
+}
