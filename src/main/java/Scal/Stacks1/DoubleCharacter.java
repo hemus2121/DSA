860f3b10bea37810cd149458cf578527aca30657
hemus2121 <hemus2121@github.com>
2022-02-03 22:50:51 +0530

    revising

--- src/main/java/Scal/Stacks1/DoubleCharacter.java
+++ src/main/java/Scal/Stacks1/DoubleCharacter.java
@@ -6,11 +6,8 @@ import java.util.Stack;
 Problem Description
 
 You are given a string A.
-
 An operation on the string is defined as follows:
-
 Remove the first occurrence of same consecutive characters. eg for a string "abbcd", the first occurrence of same consecutive characters is "bb".
-
 Therefore the string after this operation will be "acd".
 
 Keep performing this operation on the string until there are no more occurrences of same consecutive characters and return the final string.
--- src/main/java/Scal/Stacks1/PassingGame.java
+++ src/main/java/Scal/Stacks1/PassingGame.java
@@ -2,6 +2,22 @@ package Scal.Stacks1;
 
 import java.util.Stack;
 
+/*
+Problem Description
+
+There is a football event going on in your city. In this event, you are given A passes and players having ids between 1 and 106.
+Initially some player with a given id had the ball in his possession. You have to make a program to display the id of the player who possessed the ball after exactly A passes.
+There are two kinds of passes:
+1) ID
+2) 0
+
+For the first kind of pass, the player in possession of the ball passes the ball "forward" to
+
+player with id = ID.
+For the second kind of a pass, the player in possession of the ball passes the ball back to the player who had forwarded the ball to him.
+In the second kind of pass "0" just means Back Pass.
+Return the ID of the player who currently posseses the ball.
+ */
 public class PassingGame {
 
     public static int solve(int A, int B, int[] C) {
