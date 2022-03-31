f33587674655358c3f44dc33c15fb0326496e67a
Hemanth <hemanth@Hemanths-MacBook-Pro.local>
2022-03-31 20:04:08 +0530

    grph-BFS

--- src/main/java/Scal/Graph/BreadthFirstSearch.java
+++ src/main/java/Scal/Graph/BreadthFirstSearch.java
@@ -1,32 +1,36 @@
 package Scal.Graph;
 
-/* Sample program for BFS using Adjacency Matrix */
+/* Sample program for BFS using Adjacency List */
+
 import java.util.*;
 
 public class BreadthFirstSearch {
+
+    //NOTE: Graph is starting from node 1 and searching from 1 to 4
+
     static  boolean validPath(int n, int [][] edges, int start, int end){
         List<List<Integer>> adjacency_list = new ArrayList<>();
 
         //building outer list for  adjacency list
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i <= n; i++) {
             adjacency_list.add(new ArrayList<>());
         }
 
-        // building list with each edges
+        // building list with each edges- - undirected graph representation
         for (int[] edge : edges) {
             adjacency_list.get(edge[0]).add(edge[1]);
             adjacency_list.get(edge[1]).add(edge[0]);
         }
 
-        
+
         Queue<Integer> queue = new LinkedList<>();
         queue.add(start);
-        boolean visited[] = new boolean[n];
-        Arrays.fill(visited, false);
+
+        // to keep track of visited nodes
+        boolean visited[] = new boolean[n+1];
         visited[start] = true;
 
         while (!queue.isEmpty()) {
-            // Get the current node.
             int node = queue.poll();
 
             // Check if we have reached the target node.
@@ -34,7 +38,7 @@ public class BreadthFirstSearch {
                 return true;
             }
 
-            // Add all neighbors to the stack.
+            // Add all neighbors to the stack. - iterating inner list of list
             for (int neighbor : adjacency_list.get(node)) {
                 // Check if neighbor has been added to the queue before.
                 if (!visited[neighbor]) {
@@ -43,18 +47,17 @@ public class BreadthFirstSearch {
                 }
             }
         }
-
         return false;
     }
+
+
     public static void main(String[] args) {
         int [] [] edges= {
-                {0,1},
-                {0,2},
                 {1,2},
-                {2,0},
+                {1,3},
                 {2,3},
-                {3,3}
+                {3,4},
         };
-        System.out.println(validPath(4, edges, 0, 3));
+        System.out.println(validPath(4, edges, 1, 4));
     }
 }
