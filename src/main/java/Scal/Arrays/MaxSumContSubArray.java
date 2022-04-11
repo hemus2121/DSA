1e0b26b2f4dcaff6f16aa59f4013ff78c54b76e5
Hemanth <hemanth@Hemanths-MacBook-Pro.local>
2022-04-11 23:07:30 +0530

    kadanes Algo

---
+++ src/main/java/Scal/Arrays/MaxSumContSubArray.java
@@ -0,0 +1,28 @@
+package Scal.Arrays;
+
+/*
+ Max Sum Contiguous Subarray
+
+Problem Description
+Find the contiguous non-empty subarray within an array, A of length N, with the largest sum.
+ */
+
+//Using KADAN'Es Algo
+public class MaxSumContSubArray {
+    private static int computeSumUsingKadanes(int [] A){
+
+        int ans = Integer.MIN_VALUE;
+        int sum =0;
+        for (int i =0;i<A.length;i++){
+            sum += A[i];
+            ans = Math.max(ans, sum);
+            //Reset sum = 0 if anytime sum <0
+            if (sum <0 ) sum =0;
+        }
+        return  ans;
+    }
+    public static void main(String[] args) {
+        int [] A = {1, 2, 3, 4, -10};
+        System.out.println(computeSumUsingKadanes(A));
+    }
+}
