0a4d3273c3530daa29b30fb1ea6eea87b45f9474
Hemanth <hemanth@Hemanths-MacBook-Pro.local>
2022-04-15 19:10:49 +0530

    minor revision to floyd algo

--- src/main/java/Scal/Graph/FloydWarshallAlgo.java
+++ src/main/java/Scal/Graph/FloydWarshallAlgo.java
@@ -1,4 +1,5 @@
 package Scal.Graph;
+
 /*
 Problem Description
 
@@ -9,37 +10,41 @@ If there is no possible path from vertex i to vertex j , B[i][j] = -1
 Note: Rows are numbered from top to bottom and columns are numbered from left to right.
  */
 
+import java.util.Arrays;
+
 public class FloydWarshallAlgo {
     private static int [][] computeFloydAlgo (int [][]A){
 
         // create placeholder for distance matrix
         int row = A.length;
         int col = A[0].length;
+        int DUMMY_VALUE = 1000000000;
 
+        //adjacency matrix to have distance between all the nodes
         int [][] dist = new int[row][col];
 
-        //build adj matrix
-        for (int i=0;i< row;i++){
-            for (int j =0;j <col; j++){
-                if (A[i][j]==-1){
-                    dist [i] [j] = (int)(1e9);
-                }else dist[i][j] = A[i][j];
+        //build adj matrix from input matrix
+        for (int i=0; i< row;i++){
+            for (int j =0; j <col; j++){
+                // if input value is -1 setting dummy value to the cell
+                if (A[i][j] == -1) dist [i] [j] = DUMMY_VALUE;
+                else dist[i][j] = A[i][j];
             }
         }
 
-        // compute using algo
-        for (int k =0;k< row ;k++){
-            for (int i =0;i< row;i ++){
-                for (int j =0;j< col; j++){
+        // compute using Floyds algo
+        for (int k =0; k< row ;k++){
+            for (int i =0; i< row;i ++){
+                for (int j =0; j< col; j++){
                     dist [i][j] = Math.min(dist[i][j], dist[i][k]+ dist[k][j]);
                 }
             }
         }
 
-        // revamp output array
+        // Check if any of the cell value not revised, set it to -1 as mentioned in question
         for (int i =0;i< row;i++){
             for (int j =0;j< col;j++){
-                if (dist[i][j] == (int)(1e9)){
+                if (dist[i][j] == DUMMY_VALUE){
                     dist [i][j]= -1;
                 }
             }
@@ -48,14 +53,10 @@ public class FloydWarshallAlgo {
     }
 
     public static void main(String[] args) {
-
         int [] [] A = {
                 {0, 50, 39},
                 {-1, 0, 1},
-                {-1, 10, 0}
-        };
-
-        System.out.println(computeFloydAlgo( A));
-
+                {-1, 10, 0} };
+        System.out.println(Arrays.toString(computeFloydAlgo(A)));
     }
 }
