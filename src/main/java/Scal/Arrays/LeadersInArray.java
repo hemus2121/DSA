17733c2066ed45ebcdad85c06ae3cfba966a6bdd
Hemanth <hemanth@Hemanths-MacBook-Pro.local>
2022-04-23 17:06:35 +0530

    Array Prob- leader

---
+++ src/main/java/Scal/Arrays/LeadersInArray.java
@@ -0,0 +1,41 @@
+package Scal.Arrays;
+
+/*
+Problem Description
+
+Given an integer array A containing N distinct integers, you have to find all the leaders in array A.
+An element is a leader if it is strictly greater than all the elements to its right side.
+NOTE:The rightmost element is always a leader.
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LeadersInArray {
+    private static int [] getLeaders(int [] data){
+
+        int n = data.length;
+        // result List
+        List<Integer> resultList = new ArrayList();
+
+        //rightmost element is always leader
+        int greaterElement = data[n-1];
+        resultList.add( greaterElement);
+
+        //iterate from right and maintain a right greater element till now
+        for (int i = n-2;i>=0;i--){
+            if ( data[i] > greaterElement){
+                greaterElement = data[i];
+                resultList.add(greaterElement);
+            }
+        }
+
+        return resultList.stream().mapToInt(val->val).toArray();
+    }
+
+    public static void main(String[] args) {
+        int [] data = { 16, 17, 4, 3, 5, 2};
+        System.out.println(Arrays.toString(getLeaders(data)));
+    }
+}
