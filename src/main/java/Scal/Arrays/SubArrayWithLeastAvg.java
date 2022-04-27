dff2fc35147302801d8cfaed6ce2cd93cd10ead1
Hemanth <hemanth@Hemanths-MacBook-Pro.local>
2022-04-27 16:16:53 +0530

    SubArrayWithLeastAvg

---
+++ src/main/java/Scal/Arrays/SubArrayWithLeastAvg.java
@@ -0,0 +1,27 @@
+package Scal.Arrays;
+
+public class SubArrayWithLeastAvg {
+    static int getIndexOfSubArray(int [] A, int B){
+
+        int currSum=0;
+        //compute sum for 1st window - SLIDING WINDOW
+        for(int i =0;i< B;i++){
+            currSum += A[i];
+        }
+
+        int minsum = currSum;
+        int index=0;
+        for (int i =1;i<= A.length-B;i++){
+            currSum = currSum - A[i-1] + A[i+B-1];
+            if (currSum< minsum){
+                minsum= currSum;
+                index=i;
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args) {
+        int [] data = { 3, 7, 90, 20, 10, 50, 40 };
+        System.out.println(getIndexOfSubArray(data, 3));
+    }
+}
