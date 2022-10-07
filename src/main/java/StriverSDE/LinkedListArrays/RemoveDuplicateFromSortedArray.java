f03b9bac7f1b8f63747413603e63788f7ebd98c4
hemanth <hemus2121@gmail.com>
2022-10-07 12:38:02 +0530

    Striver-SDE-Arrays-Probs

--- src/main/java/StriverSDE/LinkedListArrays/RemoveDuplicateFromSortedArray.java
+++ src/main/java/StriverSDE/LinkedListArrays/RemoveDuplicateFromSortedArray.java
@@ -2,7 +2,6 @@ package StriverSDE.LinkedListArrays;
 
 /** Remove Duplicates in-place from Sorted Array
  * Problem Statement: Given an integer array sorted in non-decreasing order, remove the duplicates in place such that each unique element appears only once. The relative order of the elements should be kept the same.
- *
  * If there are k elements after removing the duplicates, then the first k elements of the array should hold the final result. It does not matter what you leave beyond the first k elements.
  *
  * Note: Return k after placing the final result in the first k slots of the array.
@@ -10,8 +9,8 @@ package StriverSDE.LinkedListArrays;
 public class RemoveDuplicateFromSortedArray {
     private static int  removeDuplicateFromSortedArray(int [] nums){
         //solve using 2 pointers
-        // - i moves slowly and tells where to insert new character
-        // -- j moves and check for new incoming character
+        // i moves slowly and tells where to insert new character
+        // j moves and check for new incoming character
         int i =0;
         for (int j =1;j< nums.length;j++){
             // if new number has come put that in updated ith position
--- src/main/java/StriverSDE/LinkedListArrays/TrappingRainWater.java
+++ src/main/java/StriverSDE/LinkedListArrays/TrappingRainWater.java
@@ -1,13 +1,14 @@
 package StriverSDE.LinkedListArrays;
 
 /** Trapping Rainwater
+ *  YT link - https://www.youtube.com/watch?v=m18Hntz4go8&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=43
  * Problem Statement: Given an array of non-negative integers representation elevation of ground. Your task is to find the water that can be trapped after rain.
  */
 public class TrappingRainWater {
 
-    //Time Complexity: O(3*N) as we are traversing through the array only once. And O(2*N) for computing prefix and suffix array.
-    //Space Complexity: O(N)+O(N) for prefix and suffix arrays.
-    private static int getTrappedWater(int [] A){
+    //TC: O(3*N) as we are traversing through the array only once. And O(2*N) for computing prefix and suffix array.
+    //SC: O(N)+O(N) for prefix and suffix arrays.
+    private static int getTrappedWaterExtraSpace(int [] A){
 
         int n = A.length;
         //declare 2 array to hold left max and right max
@@ -33,8 +34,36 @@ public class TrappingRainWater {
         }
         return totalWater;
     }
+
+    //Using 2 pointer approach
+    // TC:O(N) - 2 pointer approach
+    // SC:O(1)
+    private static int getTrappedWaterOptimal (int []height){
+        int leftMax=0, rightMax=0, res=0;
+        int left =0, right = height.length-1;
+
+        while (left < right ){
+             // 2 cases arise
+            if (height[left] <= height[right]){
+                if (height[left]>=leftMax){ // check if current left is maximum till now if YES - remaining leftMax value
+                    leftMax = height[left];
+                }else res += leftMax- height[left];
+
+                 left++;
+            } else {
+                if (height[right] >= rightMax){
+                    rightMax = height[right]; // check if current right is maximum till now if YES - remaining leftMax value
+                }else {
+                    res += rightMax- height[right];
+                }
+                right --;
+            }
+        }
+        return res;
+    }
     public static void main(String[] args) {
         int[] A = {0,1,0,2,1,0,1,3,2,1,2,1};
-        System.out.println(getTrappedWater(A)); //expected 6
+        System.out.println(getTrappedWaterExtraSpace(A)); //expected 6
+        System.out.println(getTrappedWaterOptimal(A)); //expected 6
     }
 }
