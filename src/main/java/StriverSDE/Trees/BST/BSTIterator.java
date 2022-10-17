87d9dc00e972e23ca6eba461dd99b9c1cd003554
hemanth <hemus2121@gmail.com>
2022-10-17 20:16:27 +0530

    Striver-SDE-Trees-BST

---
+++ src/main/java/StriverSDE/Trees/BST/BSTIterator.java
@@ -0,0 +1,34 @@
+package StriverSDE.Trees.BST;
+
+import StriverSDE.Trees.Node;
+
+import java.util.Stack;
+
+/** BST Iterator
+ * Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+ */
+public class BSTIterator {
+
+    Stack<Node> st = new Stack<>();
+    public BSTIterator(Node root) {
+        pushAll(root);
+    }
+
+    public int next() {
+        Node tmpNode = st.pop();
+        while (tmpNode.right!= null){
+            pushAll(tmpNode.right);
+        }
+        return tmpNode.data;
+    }
+
+    public boolean hasNext() {
+        return !st.isEmpty();
+    }
+    private void pushAll(Node root){
+        while (root != null){
+            st.push(root);
+            root = root.left;
+        }
+    }
+}
