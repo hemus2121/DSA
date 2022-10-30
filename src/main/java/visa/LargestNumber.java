c73580167d32abbb7dd76f02f087106dcfecae62
hemanth <hemus2121@gmail.com>
2022-10-30 10:52:30 +0530

    visa-strings

---
+++ src/main/java/visa/LargestNumber.java
@@ -0,0 +1,35 @@
+package visa;
+
+//https://leetcode.com/problems/largest-number/
+import java.util.Arrays;
+
+public class LargestNumber {
+
+    public static  String largestNumber(int[] nums) {
+
+        //create string array by integer array
+        String [] data = new String[nums.length];
+        int index=0;
+        for (int i : nums){
+            data[index++] = String.valueOf(i);
+        }
+
+        // sort the array
+        Arrays.sort(data, (s1,s2)-> (s2+s1).compareTo(s1+s2));
+
+        //If 1st character is zero return 0
+        if (data[0].equalsIgnoreCase("0")) return "0";
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : data){
+            sb.append(s);
+        }
+        return  sb.toString();
+    }
+
+    public static void main(String[] args) {
+        int [] nums ={ 3, 30, 34, 5, 9};
+        System.out.println(largestNumber(nums));
+    }
+
+}
---
+++ src/main/java/visa/RemoveAllAdjDuplicateElements.java
@@ -0,0 +1,41 @@
+package visa;
+
+import java.util.Stack;
+// https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/
+
+public class RemoveAllAdjDuplicateElements {
+
+    //using Pair object
+    static class Pair {
+        char c ;
+        int count;
+        Pair(char c , int count){
+            this.c = c;
+            this.count = count;
+        }
+    }
+    public static String removeDuplicates(String s, int k) {
+        Stack<Pair> st = new Stack<>();
+        for (int i =0;i <s.length();i++){
+            if (st.isEmpty() || s.charAt(i)!= st.peek().c){
+                st.push (new Pair(s.charAt(i),1));
+            }else {
+                if (++st.peek().count==k){
+                    st.pop();
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!st.isEmpty()){
+            Pair p =st.pop();
+            for (int i =0;i< p.count;i++){
+                sb.append(p.c);
+            }
+        }
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates("deeedbbcccbdaa", 3));
+    }
+}
